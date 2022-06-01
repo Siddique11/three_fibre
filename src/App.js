ef2dabd410f9b6add0fa683ec022c8bd7870c402
Siddique11 <absiddique10@yahoo.com>
2022-06-01 11:45:38 +0100

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+// import { Canvas, extend } from "@react-three/fiber";
+// import { shaderMaterial } from "@react-three/drei";
+// import glsl from "babel-plugin-glsl/macro";
 
-function App() {
+import * as THREE from "three";
+import React, { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
+import flag from "./Flag_of_Bangladesh.svg.png";
+import "./App.css";
+
+const WaveShaderMaterial = shaderMaterial(
+  // Uniform
+  {
+    uTime: 0,
+    uColor: new THREE.Color(1.0, 0.0, 0.0),
+    uTexture: new THREE.Texture(),
+  },
+  // Vertex Shader
+  glsl`
+  precision mediump float;
+  varying vec2 vUv;
+  varying float vWave;
+
+  uniform float uTime;
+
+
+  #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
+   void main() {
+     vUv = uv;
+     vec3 pos = position ;
+     float noiseFreq = 2.5;
+     float noiseAmp =  0.55;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+
+      vWave = pos.z;
+
+
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);  
+  }
+  `,
+  // Fragment Shader
+  glsl` 
+  precision mediump float;
+
+  uniform vec3 uColor;
+  uniform float uTime;
+  uniform sampler2D uTexture;
+
+  varying vec2 vUv;
+  varying float vWave;
+
+   void main() {
+     float wave = vWave * 0.1 ;
+     vec3 texture = texture2D(uTexture, vUv + wave).rgb;
+    gl_FragColor = vec4(texture, 1.0);
+  }
+  `
+);
+
+extend({ WaveShaderMaterial });
+
+const Wave = () => {
+  const ref = useRef();
+  // useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  const [image] = useLoader(THREE.TextureLoader, [flag]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh>
+      <planeBufferGeometry args={[0.6, 0.4, 16, 16]} />
+      <waveShaderMaterial uColor={"hotpink"} ref={ref} uTexture={image} />
+    </mesh>
   );
-}
+};
+
+const Scene = () => {
+  return (
+    <Canvas camera={{ fov: 12, position: [0, 0, 5] }}>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <h1>Bangladesh</h1>
+      <Scene />
+    </>
+  );
+};
 
 export default App;
